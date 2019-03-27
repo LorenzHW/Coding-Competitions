10d0bb196a6bca598517f37f7f884f3834c812be
LorenzHW <lorenzhofmann.w@gmail.com>
2019-03-27 18:27:22 +0100

    kickstart round a analysis

---
+++ kickstart/2019/RoundA/1/trainin_analysis.py
@@ -0,0 +1,37 @@
+def minimum_coaching(N, P, S):
+    S.sort(reverse=True)
+    
+    min_training_time = float('inf')
+
+    # Training time formula for subarray starting at position i is as follows:
+    # = Σ(S[i] - S[j]) where j = i to i + P -1 
+    # = P × S[i] - Σ(S[j]) where j = i to i + P - 1 
+    prefix_sum = [sum(S[:P])]
+    for i in range(P, N): 
+        prefix_sum.append(prefix_sum[-1] + S[i] - S[i - P])
+
+    for i in range(N - P + 1):
+        training_time_team = S[i] * P - prefix_sum[i]
+        min_training_time = min(min_training_time, training_time_team) 
+    return min_training_time
+
+
+
+# number_of_test_cases = int(input())
+# for i in range(1, number_of_test_cases + 1):
+#     N, P = input().split()
+#     N = int(N)
+#     P = int(P)
+#     S = input().split()
+#     S = list(map(int, S))
+#     result = minimum_coaching(N, P, S)
+#     print("Case #{}: {}".format(i, result))
+
+if __name__ == "__main__":
+    P = 3
+    S = [3, 1, 9, 100]
+    # S = [5, 5, 1, 2, 3, 4]
+    N = len(S)
+    print(minimum_coaching(N, P, S))
+    
+    
---
+++ kickstart/2019/RoundA/1/training.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+
+def minimum_coaching(N, P, S):
+    S.sort()
+    current_team = deque(S[:P]) 
+    res = hours_of_coaching(current_team)
+
+    for skill in S[P:]:
+        current_team.append(skill)
+        current_team.popleft()
+        
+        time_needed = hours_of_coaching(current_team)
+        if time_needed < res:
+            res = time_needed
+    return res
+
+# student_skills is of size P and is sorted
+def hours_of_coaching(student_skills):
+    max_skill = student_skills[-1]
+    hours_of_coaching = 0
+
+    for skill in student_skills:
+        hours_of_coaching += max_skill - skill
+    return hours_of_coaching
+
+
+# number_of_test_cases = int(input())
+# for i in range(1, number_of_test_cases + 1):
+#     N, P = input().split()
+#     N = int(N)
+#     P = int(P)
+#     S = input().split()
+#     S = list(map(int, S))
+#     result = minimum_coaching(N, P, S)
+#     print("Case #{}: {}".format(i, result))
+
+if __name__ == "__main__":
+    N = 4
+    P = 3
+    S = [3, 1, 9, 100]
+    print(minimum_coaching(N, P, S))
+    
+    
