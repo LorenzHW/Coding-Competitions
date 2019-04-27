3b42f86626125ae98d75cb8b88bee14fdb52ad7e
LorenzHW <lorenzhofmann.w@gmail.com>
2019-04-27 11:41:46 +0200

    code jam round 1c 2018 practice

--- code_jam/2018/round_1-c/1_problem/whole_new_world.py
+++ code_jam/2018/round_1-c/1_problem/whole_new_world.py
@@ -1,32 +1,61 @@
-def whole_new_world(N, L, words):
-    letters_lists = [[] for _ in range(L)]
-    for i in range(N):
-        for j in range(L):
-            s = letters_lists[j]
-            s.append(words[i][j])
+from collections import defaultdict
 
+
+def find_different_suffix(grouped):
+    """
+    Checks whether two different suffixes exist
+    If so, the prefixes of those suffixes are returned
+    :param grouped: splitted words
+    :return: Prefixes of different suffixes if they exist otherwise none
+    """
+    items = list(grouped.items())
+    prefix, suffixes = items[0]
+    for other_prefix, other_suffixes in items[1:]:
+        if suffixes != other_suffixes:
+            return prefix, other_prefix
+    return None, None
+
+
+def diff(set1, set2):
+    """
+    Checks if the suffix of set1 is in set2
+    :param set1: set of suffixes
+    :param set2: set of suffixes
+    :return: A suffix that is in set2 but not in set1
+    """
+    for suffix in set2:
+        if suffix not in set1:
+            return suffix
+
+
+def generate_word(grouped):
+    prefix, other_prefix = find_different_suffix(grouped)
+    if prefix is None:
+        # All suffixes are the same --> not possible to to generate a different word
+        return None
+
+    # Ok, so we know a different suffix exists. Does it exist in
+    # grouped[other_prefix]
+    other_suffix = diff(grouped[prefix], grouped[other_prefix])
+    if other_suffix:
+        return prefix + other_suffix
+
+    # Looks like it does not. So it has to exist in grouped[prefix]
+    other_suffix = diff(grouped[other_prefix], grouped[prefix])
+    return other_prefix + other_suffix
+
+
+def whole_new_world(L, words):
     words = set(words)
 
-    def generate_word(current_word, letters_lists_idx):
-        if len(current_word) == L:
-            current_word = ''.join(current_word)
-            if current_word not in words:
-                return current_word
-            else:
-                return None
-
-        letter_list = letters_lists[letters_lists_idx]
-
-        for letter in letter_list:
-            word = generate_word(current_word + [letter], letters_lists_idx + 1)
-            if word:
-                return word
-
-    word = generate_word([], 0)
-    if word:
-        return word
-    else:
-        return '-'
+    for i in range(1, L):
+        grouped = defaultdict(set)
+        for word in words:
+            grouped[word[:i]].add(word[i:])
+        w = generate_word(grouped)
+        if w is not None:
+            return w
+    return "-"
 
 
 number_of_test_cases = int(input())
@@ -38,17 +67,30 @@ for i in range(1, number_of_test_cases + 1):
     for _ in range(N):
         words.append(input())
 
-    res = whole_new_world(N, L, words)
+    res = whole_new_world(L, words)
     print("Case #{}: {}".format(i, res))
 
+
 def main():
-    N = 3
-    L = 4
+    L = 2
     words = ["AA", "AB", "BA", "BB"]
+
+    # L = 1
     # words = ["A", "B", "C", "D"]
-    words = ["CAKE", "TORN", "SHOW"]
+    #
+    # L = 4
+    # words = ["CAKE", "TORN", "SHOW"]
+    #
+    # L = 2
     # words = ["WW", "AA", "SS", "DD"]
-    print(whole_new_world(N, L, words))
+    #
+    # L = 7
+    # words = ["HELPIAM", "TRAPPED", "INSIDEA", "CODEJAM", "FACTORY"]
+    #
+    # L = 2
+    # words = ["TT", "TT"]
+
+    print(whole_new_world(L, words))
 
 
 if __name__ == "__main__":
